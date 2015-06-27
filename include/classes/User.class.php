c5986721547c3f534534f673bc3d64e6a67b94c9
Dante <dante.stoppini@gmail.com>
2015-06-27 12:02:14 +0200

    Added Post and User class

---
+++ include/classes/User.class.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ *  Userclass
+ *  @author happyoniens
+ */
+class User {
+    /**
+     * Userid
+     * @var String
+     * @example b4148d18
+     */
+    private $userid;
+    /**
+     * Username
+     * @var String
+     */
+    private $username;
+    /**
+     * Password encrypted with SHA 256
+     * @var String
+     */
+    private $password;
+    /**
+     * Email
+     * @var String
+     */
+    private $email;
+
+    function __construct($arr){
+        $this->userid = $arr['userid'];
+        $this->username = $arr['username'];
+        $this->password = $arr['password'];
+        $this->email = $arr['email'];
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Userid
+     *
+     * @return String
+     */
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     * Set the value of Userid
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * Get the value of Username
+     *
+     * @return String
+     */
+    public function getUsername(){
+        return $this->username;
+    }
+
+    /**
+     * Set the value of Username
+     */
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    /**
+     * Get the value of Password
+     *
+     * @return String
+     */
+    public function getPassword(){
+        return $this->password;
+    }
+
+    /**
+     * Set the value of Password
+     */
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    /**
+     * Get the value of Email
+     *
+     * @return String
+     */
+    public function getEmail(){
+        return $this->email;
+    }
+
+    /**
+     * Set the value of Email
+     */
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+}
+?>
