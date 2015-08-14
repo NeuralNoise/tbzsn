8b5a0dde2b16578483ced900740198780250481f
Acid147 <offacid@gmail.com>
2015-08-14 19:25:10 +0200

    __get & __set in models

--- models/Post.php
+++ models/Post.php
@@ -41,134 +41,22 @@ class Post {
     */
     private $creationdate;
 
-    function initialize($arr){
-        $this->id = $arr['postid'];
-        $this->visibility = $arr['visibility'];
-        $this->likes = $arr['likes'];
-        $this->owner = $arr['owner'];
-        $this->groupid = $arr['groupid'];
-        $this->type = $arr['type'];
+    function initialize($arr) {
+        $this->id           = $arr['postid'];
+        $this->visibility   = $arr['visibility'];
+        $this->likes        = $arr['likes'];
+        $this->owner        = $arr['owner'];
+        $this->groupid      = $arr['groupid'];
+        $this->type         = $arr['type'];
         $this->creationdate = $arr['creationdate'];
         return $this;
     }
-    /**
-     * Get the value of Postid
-     *
-     * @return String
-     */
-    public function getPostid(){
-        return $this->postid;
+
+    public function __set($name, $value) {
+        $this->$name = $value;
     }
 
-    /**
-     * Set the value of Postid
-     */
-    public function setVisibility($postid) {
-        $this->postid = $postid;
+    public function __get($name) {
+        return $this->$name;
     }
-
-    /**
-     * Get the value of Visibility of Post
-     *
-     * @return int
-     */
-    public function getVisibility(){
-        return $this->visibility;
-    }
-
-    /**
-     * Set the value of Visibility of Post
-     */
-    public function setVisibility($visibility) {
-        $this->visibility = $visibility;
-    }
-
-    /**
-     * Get the value of Count of likes
-     *
-     * @return int
-     */
-    public function getLikes(){
-        return $this->likes;
-    }
-
-    /**
-     * Set the value of Count of likes
-     */
-    public function setLikes($likes) {
-        $this->likes = $likes;
-    }
-
-    /**
-     * Get the value of Userid from the Postowner
-     *
-     * @return String
-     */
-    public function getOwner(){
-        return $this->owner;
-    }
-
-    /**
-     * Set the value of Userid from the Postowner
-     */
-    public function setOwner($owner) {
-        $this->owner = $owner;
-    }
-
-    /**
-     * Get the value of Groupid
-     *
-     * @return String
-     */
-    public function getGroupid(){
-        return $this->groupid;
-    }
-
-    /**
-     * Set the value of Groupid
-     */
-    public function setGroupid($groupid) {
-        $this->groupid = $groupid;
-    }
-    /**
-     * Set the value of Userid from the Postowner
-     */
-    public function setOwner($owner) {
-        $this->owner = $owner;
-    }
-
-    /**
-     * Get the value of Type
-     *
-     * @return Int
-     */
-    public function getType(){
-        return $this->type;
-    }
-
-    /**
-     * Set the value of Type
-     */
-    public function setType($type) {
-        $this->type = $type;
-    }
-
-    /**
-     * Get the value of Creationdate
-     *
-     * @return Date
-     */
-    public function getCreationdate(){
-        return $this->creationdate;
-    }
-
-    /**
-     * Set the value of Creationdate
-     */
-    public function getCreationdate($creationdate) {
-        $this->creationdate = $creationdate;
-    }
-
-
 }
- ?>
--- models/User.php
+++ models/User.php
@@ -27,77 +27,19 @@ class User {
     private $email;
 
     function initialize($arr){
-        $this->userid = $arr['userid'];
+        $this->userid   = $arr['userid'];
         $this->username = $arr['username'];
         $this->password = $arr['password'];
-        $this->email = $arr['email'];
+        $this->email    = $arr['email'];
 
         return $this;
     }
 
-    /**
-     * Get the value of Userid
-     *
-     * @return String
-     */
-    public function getId(){
-        return $this->id;
+    public function __set($name, $value) {
+        $this->$name = $value;
     }
 
-    /**
-     * Set the value of Userid
-     */
-    public function setId($id) {
-        $this->id = $id;
+    public function __get($name) {
+        return $this->$name;
     }
-
-    /**
-     * Get the value of Username
-     *
-     * @return String
-     */
-    public function getUsername(){
-        return $this->username;
-    }
-
-    /**
-     * Set the value of Username
-     */
-    public function setUsername($username) {
-        $this->username = $username;
-    }
-
-    /**
-     * Get the value of Password
-     *
-     * @return String
-     */
-    public function getPassword(){
-        return $this->password;
-    }
-
-    /**
-     * Set the value of Password
-     */
-    public function setPassword($password) {
-        $this->password = $password;
-    }
-
-    /**
-     * Get the value of Email
-     *
-     * @return String
-     */
-    public function getEmail(){
-        return $this->email;
-    }
-
-    /**
-     * Set the value of Email
-     */
-    public function setEmail($email) {
-        $this->email = $email;
-    }
-
 }
-?>
